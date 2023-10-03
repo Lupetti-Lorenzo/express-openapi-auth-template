fdcb525e548080d12a6071d9a179817a099f5cd0
Lorenzo Lupetti <lorenzolupetti@Lorenzos-MacBook-Pro.local>
2023-10-03 20:31:41 +0200

    Fixed redisrepo, now i am using the async functions built in and the login successfully stores the token in the redis cache

--- src/repos/RedisRepo.ts
+++ src/repos/RedisRepo.ts
@@ -1,38 +1,32 @@
 import { RedisClientOptions, createClient } from 'redis';
-import { promisify } from 'util';
 import EnvVars from '@src/constants/EnvVars';
 
 // Initialize the Redis client
 const { Host, Port } = EnvVars.Redis;
 const url = `redis://${Host}:${Port}`;
 const options: RedisClientOptions = { url };
-const client = createClient(options);
-
-client.on('error', (err) => {
-	console.log(`>>>> Redis Error: ${err}`);
-});
+const client = createClient(options).on('error', (err) => console.log('Redis Client Error', err));
 
 console.log(`<<<< Connected to Redis >>>>`);
 
-// Promisified Redis functions
-const getAsync = promisify(client.get).bind(client);
-const setAsync = promisify(client.set).bind(client);
-const delAsync = promisify(client.del).bind(client);
-
 // Set a token in Redis by ID with an expiration time (in milliseconds)
-export async function setTokenById(id: string, token: string, expirationInMilliseconds: number): Promise<void> {
-	await client.connect().catch((err) => console.log(err));
-	await setAsync(id, token, 'PX', expirationInMilliseconds);
+export async function setTokenById(id: string, token: string, expirationInMilliseconds: number): Promise<string | null> {
+	await client.connect();
+	return client.set(id, token, {
+		PX: expirationInMilliseconds,
+	});
 }
 
 // Get a token from Redis by ID
 export async function getTokenById(id: string): Promise<string | null> {
-	return await getAsync(id);
+	await client.connect();
+	return client.get(id);
 }
 
 // Revoke (delete) a token from Redis by ID
-export async function revokeTokenById(id: string): Promise<void> {
-	await delAsync(id);
+export async function revokeTokenById(id: string): Promise<number> {
+	await client.connect();
+	return client.del(id);
 }
 
 export default {
